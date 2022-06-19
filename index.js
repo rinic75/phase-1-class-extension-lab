969bf2802321de31a766c05c5731a7156ad0c7e4
Chang Shin <cshin7374@gmail.com>
2022-06-19 04:41:59 -0500

    commit tree 2ebbe8e3204304028c2f61fb769030bf16475503

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.array = array;
+    this.numSide = this.array.length;
+  }
+  get countSides() {
+    return this.array.length;
+  }
+  get perimeter() {
+    let sum = 0
+    for(let i of this.array) {
+      sum += i
+    }
+    return sum
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if(this.numSide !== 3){
+      return false
+    }
+    return ((this.array[0] + this.array[1] > this.array[2]) && (this.array[1] + this.array[2] > this.array[0]) && (this.array[0] + this.array[2] > this.array[1]))
+  }
+}
+
+class Square extends Polygon {
+  get isValid () {
+    if(this.numSide !== 4) {
+      return false
+    } else {
+    return ((this.array[0]===this.array[1])&&(this.array[0]===this.array[2])&&(this.array[0]===this.array[3]))
+    }
+  }
+  get area() {
+    if(this.numSide !== 4) {
+      return false
+    } else {
+    return this.array[0] * this.array[1]
+    }
+  }
+}
+
